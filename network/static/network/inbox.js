3d06abdba833a847fe0b53805281d4823a3e1f26
Rodrigo <rodrigojmayer@gmail.com>
2022-05-16 23:11:02 +0200

    navbar-profiledropdownlist

--- network/static/network/inbox.js
+++ network/static/network/inbox.js
@@ -233,3 +233,23 @@ function close_cover(){
     document.querySelector('.cover').style.display="none";
     document.querySelector('#new_post_view').style.display = 'none';
 }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+        console.log(event.target);
+        // if (!event.target.matches('.dropbtn')) {
+        if (!event.target.matches('#dropbtn-profile')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+            }
+        }
+        }
+    }
