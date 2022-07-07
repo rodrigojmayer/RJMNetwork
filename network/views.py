43922788f3bcf00cc7cd0d73b7b09175db1d8e20
Rodrigo <rodrigojmayer@gmail.com>
2022-07-07 19:32:51 +0200

    Pages with paginator8

--- network/views.py
+++ network/views.py
@@ -33,20 +33,23 @@ def postsbox(request, filter_view, jump_page):
 
     all_posts = NewPost.objects.select_related('poster')
     all_posts = all_posts.order_by("-date_added")
-    if filter_view == "following":
-        if request.user.id:
+    if request.user.id:
+        if filter_view == "following":
             follows_filter=[]
             followers = Followers.objects.filter(follower=request.user.id)
             for each_followers_filter in followers:
                 follows_filter.append(each_followers_filter.followed.id)
             all_posts=all_posts.filter(poster__in=follows_filter)
+
+        if filter_view == "liked_posts":
+            likers_filter=[]
+            likers = Likers.objects.filter(liker=request.user.id)
+            for each_likers_filter in likers:
+                likers_filter.append(each_likers_filter.post.id)
+            all_posts = all_posts.filter(id__in=likers_filter)
     
-        else:
-            return render(request, "network/register.html")
-
-
-
-            
+    else:
+        return render(request, "network/register.html")
 
 
     p = Paginator(all_posts, 10)
@@ -204,6 +207,64 @@ def following(request):
 
 
 
+def liked_posts(request):
+
+    if request.user.id:
+        likers_filter=[]
+        likers = Likers.objects.filter(liker=request.user.id)
+        for each_likers_filter in likers:
+            # print(each_likers_filter.followed.id)
+            # print(each_likers_filter.post.id)
+            # print(each_likers_filter)
+            # likers_filter.append(each_likers_filter.followed.id)
+            likers_filter.append(each_likers_filter.post.id)
+        all_posts = NewPost.objects.all()
+        all_posts = all_posts.order_by("-date_added")
+        # all_posts2=all_posts.filter(poster__in=[15])
+        all_posts2=all_posts.filter(id__in=likers_filter)
+        # print(likers_filter)
+        # print("1--------------")
+        # print(all_posts)
+        # print("-2-------------")
+        # print(all_posts2)
+        total_posts=all_posts2.count()
+        all_posts2=all_posts2[:10]
+        total_pages=math.ceil(total_posts/10)
+        list_total_pages = []
+        for i in range(2, total_pages+1):
+            list_total_pages.append(i)
+        all_likers = Likers.objects.all()
+    
+        for post in all_posts2:
+            post.date_added = (post.date_added.strftime("%b %d, %Y, %H:%M"))
+            post.number_likes=0
+            likers = all_likers.filter(post=post.id)
+            likers_id = []
+            for each_liker in likers:
+                post.likers = each_liker.liker.all()
+                post.number_likes = each_liker.liker.count()
+                for each in each_liker.liker.all():
+                    likers_id.append(each.id)
+                post.likers_id = likers_id
+
+                
+        users = User.objects.all()
+        user_color = {}
+        colors_list = ["C37D7D", "FC792F", "4950F8", "EBFC2F", "15A2F1", "58FC2F", "36F9E1", "2ECF65", "B549F8", "FF83EB", "FCCF2F"]
+        for j in users:
+            user_color[j.id] = random.choice(colors_list)
+            colors_list.remove(user_color[j.id])
+            
+        return render(request, "network/liked_posts.html",{
+            "likers_filter":likers_filter,
+            "all_posts":all_posts2,
+            "list_total_pages":list_total_pages,
+            "user_color": user_color,
+        })
+    else:
+        return render(request, "network/register.html")
+
+
 
 def new_post(request):
     return render(request, "network/new_post.html")
@@ -308,14 +369,14 @@ def compose_post(request):
                     }, status=201)
 
 
-@csrf_exempt
-@login_required
-def postbox(request, postbox):
-    all_posts = NewPost.objects.select_related('poster')
-    return render(request, "network/index.html", {
-        "all_posts": all_posts,
-        "add_post_available": True
-    })
+# @csrf_exempt
+# @login_required
+# def post(request, postbox):
+#     all_posts = NewPost.objects.select_related('poster')
+#     return render(request, "network/index.html", {
+#         "all_posts": all_posts,
+#         "add_post_available": True
+#     })
 
 
 def profile(request, id_poster):
@@ -409,63 +470,6 @@ def follow(request, id_poster):
     }
     , status=201)
 
-
-def liked_posts(request):
-
-    if request.user.id:
-        likers_filter=[]
-        likers = Likers.objects.filter(liker=request.user.id)
-        for each_likers_filter in likers:
-            # print(each_likers_filter.followed.id)
-            # print(each_likers_filter.post.id)
-            # print(each_likers_filter)
-            # likers_filter.append(each_likers_filter.followed.id)
-            likers_filter.append(each_likers_filter.post.id)
-        all_posts = NewPost.objects.all()
-        all_posts = all_posts.order_by("-date_added")
-        # all_posts2=all_posts.filter(poster__in=[15])
-        all_posts2=all_posts.filter(id__in=likers_filter)
-        # print(likers_filter)
-        # print("1--------------")
-        # print(all_posts)
-        # print("-2-------------")
-        # print(all_posts2)
-        total_posts=all_posts2.count()
-        all_posts2=all_posts2[:10]
-        total_pages=math.ceil(total_posts/10)
-        list_total_pages = []
-        for i in range(2, total_pages+1):
-            list_total_pages.append(i)
-        all_likers = Likers.objects.all()
-    
-        for post in all_posts2:
-            post.date_added = (post.date_added.strftime("%b %d, %Y, %H:%M"))
-            post.number_likes=0
-            likers = all_likers.filter(post=post.id)
-            likers_id = []
-            for each_liker in likers:
-                post.likers = each_liker.liker.all()
-                post.number_likes = each_liker.liker.count()
-                for each in each_liker.liker.all():
-                    likers_id.append(each.id)
-                post.likers_id = likers_id
-
-                
-        users = User.objects.all()
-        user_color = {}
-        colors_list = ["C37D7D", "FC792F", "4950F8", "EBFC2F", "15A2F1", "58FC2F", "36F9E1", "2ECF65", "B549F8", "FF83EB", "FCCF2F"]
-        for j in users:
-            user_color[j.id] = random.choice(colors_list)
-            colors_list.remove(user_color[j.id])
-            
-        return render(request, "network/liked_posts.html",{
-            "likers_filter":likers_filter,
-            "all_posts":all_posts2,
-            "list_total_pages":list_total_pages,
-            "user_color": user_color,
-        })
-    else:
-        return render(request, "network/register.html")
 
 @csrf_exempt
 def pagesposts(request):
@@ -791,41 +795,34 @@ def like(request, id_post):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-def following(request):
+def liked_posts(request):
 
     if request.user.id:
-        follows_filter=[]
-        followers = Followers.objects.filter(follower=request.user.id)
-        for each_followers_filter in followers:
-            follows_filter.append(each_followers_filter.followed.id)
+        likers_filter=[]
+        likers = Likers.objects.filter(liker=request.user.id)
+        for each_likers_filter in likers:
+            # print(each_likers_filter.followed.id)
+            # print(each_likers_filter.post.id)
+            # print(each_likers_filter)
+            # likers_filter.append(each_likers_filter.followed.id)
+            likers_filter.append(each_likers_filter.post.id)
         all_posts = NewPost.objects.all()
         all_posts = all_posts.order_by("-date_added")
-        all_posts2=all_posts.filter(poster__in=follows_filter)
-
-
+        # all_posts2=all_posts.filter(poster__in=[15])
+        all_posts2=all_posts.filter(id__in=likers_filter)
+        # print(likers_filter)
+        # print("1--------------")
+        # print(all_posts)
+        # print("-2-------------")
+        # print(all_posts2)
         total_posts=all_posts2.count()
         all_posts2=all_posts2[:10]
         total_pages=math.ceil(total_posts/10)
-
-
         list_total_pages = []
         for i in range(2, total_pages+1):
             list_total_pages.append(i)
         all_likers = Likers.objects.all()
+    
         for post in all_posts2:
             post.date_added = (post.date_added.strftime("%b %d, %Y, %H:%M"))
             post.number_likes=0
@@ -837,18 +834,33 @@ def following(request):
                 for each in each_liker.liker.all():
                     likers_id.append(each.id)
                 post.likers_id = likers_id
+
+                
         users = User.objects.all()
         user_color = {}
         colors_list = ["C37D7D", "FC792F", "4950F8", "EBFC2F", "15A2F1", "58FC2F", "36F9E1", "2ECF65", "B549F8", "FF83EB", "FCCF2F"]
         for j in users:
             user_color[j.id] = random.choice(colors_list)
             colors_list.remove(user_color[j.id])
-
-        return render(request, "network/following.html",{
-            "follows_filter":follows_filter,
+            
+        return render(request, "network/liked_posts.html",{
+            "likers_filter":likers_filter,
             "all_posts":all_posts2,
             "list_total_pages":list_total_pages,
             "user_color": user_color,
         })
     else:
         return render(request, "network/register.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
