2fb2c22c0e048aaa28f6a8b801f9d697dea80b7e
Rodrigo <rodrigojmayer@gmail.com>
2022-07-26 20:53:22 +0200

    Fixed padding page buttons large

--- network/static/network/inbox.js
+++ network/static/network/inbox.js
@@ -11,28 +11,15 @@ document.addEventListener('DOMContentLoaded', function(){
     elem = document.getElementById("lookup-form");   
     // alert(elem.style.top);
     // if(elem.style.top);
-        elem.style.top = "0px";
+    elem.style.top = "0px";
     newurl = url.split('/');
-    // alert(newurl[2]);
-    
-    // if(newurl[2] == "%20")
-    //     alert("son iguales")
-    // else
-    //     alert("son distintos")
-    // window.addEventListener('load', (event) => {
-    //     console.log('page is fully loaded');
-    //   });
-    // console.log(performance.getEntriesByType("navigation")[0].type);
-
     if(newurl[2]){
-        // if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
         if (performance.getEntriesByType("navigation")[0].type == "reload") {
             newurl[2] = "%20";
             newurl = newurl.join('/');
             window.location.href = newurl;
         }else{
             if(newurl[2] != "%20"){
-                // alert("relaka")
                 document.getElementById("search").value=newurl[2];
                 document.querySelector(".search-text").focus();
                 document.querySelector('#nav-search svg path').style.fill = rs.getPropertyValue("--blue-color");
